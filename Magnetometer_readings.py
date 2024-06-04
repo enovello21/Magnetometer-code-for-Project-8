08724a790ee56b1675d2173020ee1041aa69f2d8
enovello21 <novello.eric21@gmail.com>
2024-06-04 16:23:15 -0400

    Add files via upload

---
+++ Magnetometer_readings.py
@@ -0,0 +1,27 @@
+#Project 8 Magnetometer
+# Made by Eric Novello and Professor Walter Pettus June 4 2024
+#This code is to be used alongside an arduino running off magnetometer
+#code found in the same github page as this. 
+import serial
+from serial.tools.list_ports import comports
+import datetime
+import time
+Basic_test = open("Basic_test.txt", "w")
+Basic_test.write("Magnetic fields are written in µT")
+Basic_test.write('\n')
+Basic_test.close()
+ports = [x.device for x in comports() if 'usbserial' in x.device]
+if len(ports) != 1:
+    print('ERROR: too many ports', ports)
+myserial = serial.Serial(ports[0], baudrate=115200)
+while True:
+    Basic_test = open("Basic_test.txt", "a")
+    #print(datetime.datetime.now())
+    Basic_test.write(str(datetime.datetime.now()))
+    myserial.write("\n".encode())
+    thisdata = myserial.read_until(b'XXX\r\n').decode().rstrip('\r\nXXX\r\n')
+    #print(repr(thisdata))
+    Basic_test.write(" ")
+    Basic_test.write(str(thisdata))
+    Basic_test.write('\n')
+    Basic_test.close()
